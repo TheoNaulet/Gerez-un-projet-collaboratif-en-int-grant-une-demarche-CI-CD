25647f2891a85208b46642691974ee125e99b4e2
TheoNaulet <theo.naulet@gmail.com>
2025-06-02 12:13:19 +0200

    fix: fix cors problem

--- back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
+++ back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
@@ -1,6 +1,7 @@
 package com.openclassrooms.bobapp.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.openclassrooms.bobapp.service.JokeService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("api/joke")
 public class JokeController {
 
